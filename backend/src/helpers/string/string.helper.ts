b2d50e4f8b6bed8bc7bc14ca0325dfc1fcec977e
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-20 19:56:09 +0600

    adding encryption methods

--- backend/src/helpers/string/string.helper.ts
+++ backend/src/helpers/string/string.helper.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { compareSync, hashSync } from "bcrypt";
 
 @Injectable()
 export class StringHelper {
 	public normalizer(value: string): string {
 		return value.replace(/ /g, "").toLowerCase();
+	}
+
+	public hash(value: string): string {
+		return hashSync(value, 8);
+	}
+	public compare(value: string, encrypted: string): boolean {
+		return compareSync(value, encrypted);
 	}
 }
