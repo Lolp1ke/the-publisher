9aabd734538bd9fdb230a24ee0a993a94a7ddb0e
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-21 10:02:18 +0600

    parseNumbers method

--- backend/src/helpers/object/object.helper.ts
+++ backend/src/helpers/object/object.helper.ts
@@ -2,9 +2,20 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class ObjectHelper {
-	public removeNulls(object: object) {
-		for (const item in object) {
-			if (!object[item]) delete object[item];
+	public removeNulls(object: object): object {
+		for (const key in object) {
+			if (!object[key]) delete object[key];
 		}
+
+		return object;
+	}
+
+	public parseNumbers(object: object): object {
+		for (const key in object) {
+			const parsedValue = parseInt(object[key]);
+			if (!isNaN(object[key])) object[key] = parsedValue;
+		}
+
+		return object;
 	}
 }
