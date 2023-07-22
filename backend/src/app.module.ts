8eb70f03c0392d09058dbf9c1e4f600dcc8b47e9
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-22 23:38:40 +0600

    adding new modules app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 
 import { AuthModule } from "@modules/auth/auth.module";
 import { AuthorModule } from "@modules/author/author.module";
+import { PostModule } from "@modules/post/post.module";
 
 @Module({
-	imports: [AuthModule, AuthorModule],
+	imports: [AuthModule, AuthorModule, PostModule],
 })
 export class AppModule {}
