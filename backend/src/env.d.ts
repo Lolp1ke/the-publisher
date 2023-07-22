157bd256756e066bde3321e70dd187f0277babdf
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-22 09:26:03 +0600

    adding env vars

--- backend/src/env.d.ts
+++ backend/src/env.d.ts
@@ -2,6 +2,15 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			readonly BACKEND_PORT: string;
+
+			readonly GOOGLE_ROOT_EMAIL: string;
+			readonly GOOGLE_PROJECT_ID: string;
+			readonly GOOGLE_KEY_FILE_NAME: string;
+			readonly GOOGLE_CLIENT_ID: string;
+			readonly GOOGLE_CLIENT_SECRET: string;
+			readonly GOOGLE_REDIRECT_URI: string;
+			readonly GOOGLE_REFRESH_TOKEN: string;
+			readonly GOOGLE_BUCKET_NAME: string;
 		}
 	}
 }
