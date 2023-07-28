8d9605dd9745907536419908438a7077ff244611
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-28 14:57:27 +0600

    layout fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Metadata } from "next";
 
 import "@assets/styles/default.scss";
+import "@assets/styles/general.scss";
 
 export const metadata: Metadata = {
 	title: "The publisher",
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="kk">
 			<body>{children}</body>
 		</html>
 	);
