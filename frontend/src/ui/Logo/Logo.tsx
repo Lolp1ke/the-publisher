472319a4e5a865b97dbceecd8a4c841310b5674f
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-31 19:44:33 +0600

    fixing logo redirect link

--- frontend/src/ui/Logo/Logo.tsx
+++ frontend/src/ui/Logo/Logo.tsx
@@ -9,7 +9,7 @@ interface LogoProps {
 export default function Logo({ showLocation = true }: LogoProps) {
 	return (
 		<div className="logo">
-			<Link href={"/"} className="logo__title">
+			<Link href={"/home"} className="logo__title">
 				<span className="logo__title-sub">The</span> <span className="logo__title-main">Publisher</span>
 			</Link>
 			<p className="logo__sub-title" style={{ display: showLocation ? "flex" : "none" }}>
