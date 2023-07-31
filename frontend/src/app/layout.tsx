e9bd06faa7e071d9d8416cef730dc503b8c06350
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-07-31 19:48:18 +0600

    deployment runtime error fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Metadata } from "next";
+import { Metadata, ServerRuntime } from "next";
 
 import "@assets/styles/default.scss";
 import "@assets/styles/general.scss";
@@ -7,6 +7,8 @@ import "@assets/styles/general.scss";
 export const metadata: Metadata = {
 	title: "The publisher",
 };
+
+export const runtime: ServerRuntime = "edge";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
